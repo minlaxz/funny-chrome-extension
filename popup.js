fe8672b82b7b4e05e1417d480afd2d87d82ecbe9
minlaxz <minminlaxz@gmail.com>
2020-09-27 19:51:48 +0630

    user interactive html and its js

---
+++ popup.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', function(){
+    document.getElementById('save').addEventListener('click', onclick, false)
+
+    function onclick(){
+        chrome.tabs.query({currentWindow : true , active: true}, function(tabs) {
+                // chrome.tabs.sendMessage(tabs[0].id, 'href', responseParse)
+                chrome.runtime.sendMessage({command:"post", data: "Domain" }, (response)=> {
+                    showData(response.data)
+                })
+            });
+        // chrome.tabs.getCurrent(function(tab) {
+        //     chrome.runtime.sendMessage({command:"post", data: tab.url }, (response)=> {
+        //         showData(response.data)
+        //     })
+        // });
+    }
+
+var showData = function(data) {
+    console.log('From Necromancer--', data);
+  }
+
+})
