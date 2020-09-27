a013914c18ef8508f505ea0f48019aafb892227d
minlaxz <minminlaxz@gmail.com>
2020-09-27 19:51:18 +0630

    background service.

---
+++ content.js
@@ -0,0 +1,16 @@
+//Get current domain
+
+// chrome.runtime.sendMessage({command: "fetch"}, (response) => {
+//     showData(response.data);
+//   });
+  
+  
+//   chrome.runtime.sendMessage({command: "post", data:"Test Data"}, (response) => {
+//     showData(response.data);
+//   });
+  
+
+//   var showData = function(data) {
+//     console.log('From Necromancer--', data);
+//   } "js" : ["content.js"],
+  
