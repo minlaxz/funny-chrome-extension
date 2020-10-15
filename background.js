eb4247fe637a04e0c2b0f9eaa1ffb9adf54963f5
minlaxz <minminlaxz@gmail.com>
2020-10-15 23:41:50 +0630

    storage added.

--- background.js
+++ background.js
@@ -1,60 +1,81 @@
+let active_tab_id = 0;
+let is_set = false;
+
 chrome.tabs.onActivated.addListener((tab) => {
   chrome.tabs.get(tab.tabId, (current_tab_info) => {
-    console.log('from background : ', current_tab_info.url)
-    main(current_tab_info.url)
-    // if (patt.test(current_tab_info.url)) {
-      // chrome.tabs.insertCSS(null, {file : "./mystyle.css"});
-      // chrome.tabs.executeScript(null, { file: "./foreground.js" }, () => {
-      //  console.log("i injected.");
-      // });
-    // } 
+    active_tab_id = tab.tabId;
+    console.log("bg : ", current_tab_info.url);
+    main(current_tab_info.url); /**evaluation goes here*/
+
+    // chrome.tabs.insertCSS(null, {file : "./mystyle.css"});
+    // chrome.tabs.executeScript(null, { file: "./foreground.js" }, () => { }
   });
 });
 
-chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
-  if (msg.action === "update_icon") {
-    console.log('sent from foreground {direct url} : ' , msg.value)
-    main(msg.value)
+chrome.runtime.onMessage.addListener((request, info, responseBack) => {
+  switch (request.action) {
+    case "update_status": {
+      responseBack({ code: 201 }); /**Respond back to the frontend.*/
+      var extracted = extractor(info)
+      console.log(extracted)
+      //console.log("from fg : ", request.value);
+      //console.log("inside sender", sender); /**sender has a bunch of stuff to deal, this is great, take a look! */
+      // main(request.value); /**evaluation goes here*/
+      break;
+    }
+    default: {
+      console.log("bad request !");
+    }
   }
 });
+
+var extractor = (obj) => {
+    return {
+        url : obj.url,
+        origin : obj.origin
+    }
+};
 
 var main = (url) => {
   const pattern = new RegExp("https");
-  if (pattern.test(url)) {
-    set_true();
-  } else {
-    set_false();
-  }
-}
+  pattern.test(url) ? set_true() : set_false();
+};
 
+/**
+ * testing interval, but it is bad idea.
+ * setInterval(() => { chrome.tabs.sendMessage(active_tab_id, { message: "handled from background." }); }, 3000);
+ */
 
-function set_true(){
+var set_true = () => {
   chrome.browserAction.setIcon({
     path: {
-      19: "./right19.png",
-      38: "./right38.png",
+      19: "./flags/right19.png",
+      38: "./flags/right38.png",
     },
   });
-}
+};
 
-function set_false(){
+var set_false = () => {
   chrome.browserAction.setIcon({
     path: {
-      19: "./wrong19.png",
-      38: "./wrong38.png",
+      19: "./flags/wrong19.png",
+      38: "./flags/wrong38.png",
     },
   });
-}
+};
 
+// chrome.tabs.onUpdated.addListener((callback) => {
+//   console.log(callback)
+// })
 
-// chrome.runtime.onMessage.addListener((msg, sender, resp) => {
+// chrome.runtime.onMessage.addListener((request, sender, resp) => {
 //   // chrome.tabs.query({ active: true }, function (tab) {
 //   //   console.log(tab)
 //   // })
 
-// if (msg.command == "post") {
+// if (request.command == "post") {
 //     chrome.tabs.getSelected(function (tab) {
-//       db.collection("Bookmarks").doc(msg.data).set({
+//       db.collection("Bookmarks").doc(request.data).set({
 //         url: tab.url,
 //         title: tab.title
 //       })
@@ -66,23 +87,23 @@ function set_false(){
 //         });
 //     });
 //   }
-//   if (msg.command == "fetch") {
+//   if (request.command == "fetch") {
 //     var docRef = db.collection("cities").doc("LA");
 //     docRef.get().then(function (doc) {
 //       if (doc.exists) {
 //         //doc.data()
-//         resp({ type: "result", status: "success", data: doc.data(), request: msg });
+//         resp({ type: "result", status: "success", data: doc.data(), request: request });
 //       } else {
 //         //No such document!
-//         resp({ type: "result", status: "error", data: 'No such document!', request: msg });
+//         resp({ type: "result", status: "error", data: 'No such document!', request: request });
 //       }
 //     }).catch(function (error) {
 //       //Error getting document:",error
-//       resp({ type: "result", status: "error", data: error, request: msg });
+//       resp({ type: "result", status: "error", data: error, request: request });
 //     });
 //   }
 
-//   if (msg.command == "regular") {
+//   if (request.command == "regular") {
 //     var docRef = db.collection("Bookmarks").doc("Domain");
 //     docRef.get().then((doc) => {
 //       if (doc.exists) {
--- content.js
+++ content.js
@@ -3,39 +3,36 @@ This script will be injected every page
 and call to chrome runtime API
 API, lives in background.js
 */
-function in_sync() {
-  chrome.runtime.sendMessage({
-    action: "update_icon",
-    value: window.location.href,
-  });
-}
-in_sync();
+var in_sync = () => {
+  chrome.runtime.sendMessage({ action: "update_status"},(res) => {
+      switch (res.code) {
+        case 201: {
+          set_storage();
+          break;
+        }
+        default: {
+          console.log("this is an error .-. ");
+        }
+      }
+    }
+  );
+};
+
+var set_storage = () => {
+  chrome.storage.local.set({ "a": "b" });
+  console.log("ALL SET.");
+}; //- solved
 
 /* not really need to set an interval loop.
  * var set_int = setInterval( in_sync(), 3000 );
  */
 
-/* TODO
-this wouldn't get called, when user choose
-`open in new window or new tab`
-but i really commonly used to choose
-`open in new` option alot.
-*/
-
-
-
-/* Temp solution - Not working
-// chrome.tabs.query({ currentWindow: true, active: true }, (tabs) => {
-//   chrome.tabs.sendMessage(
-//     {
-//       action: "update_icon",
-//       value: window.location.href,
-//     },
-//     //  data: tabs[0].id },
-//     (response) => {
-//       console.log("fallback response: ", response);
-//     }
-//   );
-// });
-*/
-
+/** TODO
+ * this wouldn't get called, when user choose
+ * `open in new window or new tab`
+ * but i commonly used to choose `open in new` option alot.
+ */
+in_sync();
+chrome.runtime.onMessage.addListener((request, sender, responseBack) => {
+  console.log(request.message);
+});
