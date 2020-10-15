55ff053649850e51eab1e8d841eb1a539a5c8d50
minlaxz <minminlaxz@gmail.com>
2020-10-15 13:51:19 +0630

    we work when icon is clicked.

--- popup.js
+++ popup.js
@@ -1,13 +1,16 @@
-document.addEventListener('DOMContentLoaded', function(){
+// Browser Action `popup.html controller`
+console.log('popup.js called.')
+
+document.addEventListener('DOMContentLoaded', function () {
     document.getElementById('save').addEventListener('click', onclick, false)
 
-    function onclick(){
-        chrome.tabs.query({currentWindow : true , active: true}, function(tabs) {
-                // chrome.tabs.sendMessage(tabs[0].id, 'href', responseParse)
-                chrome.runtime.sendMessage({command:"post", data: "Domain" }, (response)=> {
-                    showData(response.data)
-                })
-            });
+    function onclick() {
+        chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+            // chrome.tabs.sendMessage(tabs[0].id, 'href', responseParse)
+            chrome.runtime.sendMessage({ command: "post", data: "Domain" }, (response) => {
+                showData(response.data)
+            })
+        });
         // chrome.tabs.getCurrent(function(tab) {
         //     chrome.runtime.sendMessage({command:"post", data: tab.url }, (response)=> {
         //         showData(response.data)
@@ -15,8 +18,8 @@ document.addEventListener('DOMContentLoaded', function(){
         // });
     }
 
-var showData = function(data) {
-    console.log('From Necromancer--', data);
-  }
+    var showData = function (data) {
+        console.log('From Necromancer--', data);
+    }
 
-})
+});
