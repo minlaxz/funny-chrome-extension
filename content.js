1365c2fcbc55cdbc30f117f8a56692affc444f98
minlaxz <minminlaxz@gmail.com>
2020-10-15 13:50:55 +0630

    i am in content of webpage, inside

--- content.js
+++ content.js
@@ -1,16 +1,39 @@
 //Get current domain
 
-// chrome.runtime.sendMessage({command: "fetch"}, (response) => {
-//     showData(response.data);
-//   });
-  
-  
-//   chrome.runtime.sendMessage({command: "post", data:"Test Data"}, (response) => {
-//     showData(response.data);
-//   });
-  
+// alert(window.location.href)
+// window.open('width=600,height=600,scrollbars=no,resizable=no')
+// console.log('content.js : ', window.location.href)
 
-//   var showData = function(data) {
-//     console.log('From Necromancer--', data);
-//   } "js" : ["content.js"],
-  
+var switcher = true
+
+function f1() {
+    chrome.runtime.sendMessage({
+        action: 'updateIcon',
+        value: true
+    });
+    switcher = false
+}
+
+function f2() {
+    chrome.runtime.sendMessage({
+        action: 'updateIcon',
+        value: false
+    });
+    switcher = true
+}
+
+var setint = setInterval( function () {
+    if (switcher) {
+        f1();
+    } else {
+        f2();
+    }
+} , 3000);
+
+
+
+// chrome.tabs.query({ currentWindow : true, active: true }, function (tab) {
+//     chrome.tabs.sendMessage({command:"regular", data:tabs[0].id}, response => {
+//         console.log('fallback resopnse: ', response)
+//     })
+//   })
