7c40a7f4efcb2223e55e308f79458e20681579e3
minlaxz <minminlaxz@gmail.com>
2020-10-18 13:37:55 +0630

    sign in form changed.

--- popups/popup.js
+++ popups/popup.js
@@ -1,41 +1,68 @@
 // Browser Action `popup.html controller`
 console.log('popup.js called.')
-const methods = {
-    0 : 'test',
-    1 : 'login',
-    2 : 'logout'
-  }
+
+const r = {
+    200: 'ok.',
+    201: 'created.',
+    400: 'bad request!',
+    404: 'not found!',
+    410: 'gone'
+}
+
+const login_field = document.getElementById('login-form');
+const logout_field = document.getElementById('logout-form');
+const login_btn = document.getElementById('auth-in');
+const logout_btn = document.getElementById('auth-out');
+const output = document.getElementById('info');
+
+
+firebase.auth().onAuthStateChanged(function (user) {
+    user ? logged_in(user) : logged_out(user)
+})
+
+var logged_in = (u) => {
+    login_field.hidden = true;
+    logout_field.hidden = false;
+    output.innerText = 'as : ' + u.email
+}
+
+var logged_out = (u) => {
+    login_field.hidden = false;
+    logout_field.hidden = true;
+    output.innerText = 'as : ' + u
+}
+
 document.addEventListener('DOMContentLoaded', function () {
-    document.getElementById('auth-in').addEventListener('click', auth_in_onclick, false)
-    function auth_in_onclick() {
+
+    login_btn.addEventListener('click', onclick_auth_in, false)
+    logout_btn.addEventListener('click', onclick_auth_out, false)
+
+    function onclick_auth_in() {
         console.log('in clicked.')
         chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
-            chrome.runtime.sendMessage({ action: methods[1] }, (response) => {
-                showData(response.code)
+            chrome.runtime.sendMessage({ action: 'login', extra: tabs }, (res) => {
+                showData(res.code)
             })
         });
     }
 
-    var showData = function (code) {
-        console.log('bg return : ', code);
-    }
+    function onclick_auth_out() {
+        var c = confirm('log out : Are you sure ?')
+        if (c) {
+            chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+                chrome.runtime.sendMessage({ action: 'logout', extra: tabs }, (res) => {
+                    showData(res.code)
+                })
+            });
+        } else { window.location.reload() }
 
-});
-
-document.addEventListener('DOMContentLoaded', function () {
-    document.getElementById('auth-out').addEventListener('click', auth_out_onclick, false)
-    function auth_out_onclick() {
-        console.log('out clicked.')
-        chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
-            chrome.runtime.sendMessage({ action: methods[2] }, (response) => {
-                showData(response.code)
-            })
-        });
     }
 
     var showData = function (code) {
-        console.log('bg return : ', code);
+        console.log('bg return : ', r[code]);
     }
+
+
 
 });
 
